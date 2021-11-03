afb955bf5eaa825ea5574195389cab1b8287e4f3
SEED0228 <naokitane6828@gmail.com>
2021-11-04 02:33:07 +0900

    add check function

--- wmschedulerbot.py
+++ wmschedulerbot.py
@@ -27,7 +27,8 @@ def create_link(name, params):
     return link[:-1]
 
 
-async def show_event_information(ctx, link, params):
+async def show_event_information(ctx, params):
+    link = create_link("events", params)
     r = requests.get(
         link, headers={"Authorization": f"Basic {getenv('BASIC_AUTHORIZATION')}"}
     )
@@ -41,6 +42,41 @@ async def show_event_information(ctx, link, params):
             embed.add_field(
                 name=f"{event['title']}",
                 value=f"{event['uid'][:-20]}, {event['subject']}, {event['begin_at']}",
+                inline=False,
+            )
+    else:
+        embed = discord.Embed(
+            title="ERROR", description="Something is wrong", color=0xFF0000
+        )
+    await ctx.channel.send(embed=embed)
+
+
+async def show_progress_information(ctx, params):
+    link = create_link("events/progresses", params)
+    r = requests.get(
+        link, headers={"Authorization": f"Basic {getenv('BASIC_AUTHORIZATION')}"}
+    )
+    status = {
+        0: "todo",
+        1: "doing",
+        2: "done",
+    }
+    str = ""
+    for key in params:
+        str += (
+            f"{key}: {params[key]},"
+            if key != "status"
+            else f"{key}: {status[int(params[key])]},"
+        )
+    if r.status_code == 200:
+        data = json.loads(r.text)
+        embed = discord.Embed(
+            title=f"検索結果({params['username']})", description=str[:-1], color=0x00FF00
+        )
+        for progress in data:
+            embed.add_field(
+                name=f"{status[progress['status']]}:{progress['event']['title']}",
+                value=f"{progress['event']['uid'][:-20]}, {progress['event']['subject']}, {progress['event']['begin_at']}",
                 inline=False,
             )
     else:
@@ -84,13 +120,70 @@ async def show(ctx, args):
             embed.add_field(name=err["name"], value=err["value"], inline=False)
         await ctx.channel.send(embed=embed)
     else:
-        await show_event_information(ctx, create_link("events", params), params)
+        await show_event_information(ctx, params)
+
+
+async def check(ctx, args):
+    parmit_commands = {
+        "title": "title",
+        "subject": "subject",
+        "from": "from_deadline",
+        "to": "to_deadline",
+        "order": "order",
+        "limit": "limit",
+        "username": "username",
+        "s": "status",
+        "status": "status",
+    }
+    status = {
+        "todo": "0",
+        "doing": "1",
+        "done": "2",
+        "0": "0",
+        "1": "1",
+        "2": "2",
+    }
+    params = {"from_deadline": dt.now().strftime("%Y%m%d")}
+    errors = []
+    if len(args) <= 2:
+        errors.append({"name": "引数エラー", "value": "引数が少なすぎます"})
+    else:
+        params["username"] = args[2]
+    for arg in args[3:]:
+        attrs = arg.split("=")
+        if len(attrs) == 1:
+            params["subject"] = attrs[0]
+        elif len(attrs) == 2:
+            if attrs[0] in parmit_commands:
+                if attrs[0] in {"s", "status"}:
+                    if attrs[1] in status:
+                        attrs[1] = status[attrs[1]]
+                    else:
+                        errors.append({"name": "引数エラー", "value": "属性名が不正です"})
+                params[parmit_commands[attrs[0]]] = attrs[1]
+            else:
+                errors.append({"name": "引数エラー", "value": "属性名が不正です"})
+        else:
+            errors.append({"name": "引数エラー", "value": "=が多すぎます"})
+    errors = check_validation(params, errors)
+    if errors:
+        embed = discord.Embed(title="hoge", description="fuga", color=0xFF0000)
+        embed.title = "ERROR"
+        embed.description = "Something is wrong"
+        embed.color = 0xFF0000
+        for err in errors:
+            embed.add_field(name=err["name"], value=err["value"], inline=False)
+        await ctx.channel.send(embed=embed)
+    else:
+        await show_progress_information(ctx, params)
 
 
 # コマンド処理
 async def exec_command(ctx, args):
     if args[1] == "show":
         await show(ctx, args)
+    elif args[1] == "check":
+        await check(ctx, args)
 
 
 @client.event
